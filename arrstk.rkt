4b6e248ea2412163e03d2c477bb11dd59a4bec25
Spencer <snsallay@gmail.com>
2015-05-17 23:11:32 -0400

    create some function capability.

--- arrstk.rkt
+++ arrstk.rkt
@@ -2,9 +2,12 @@
 (require racket/list)
 
 (struct v (val type))
+(struct arr (len type)) ; for return arrays pushed to the stack.
 (define (push stk elt) (append stk (list elt)))
 (define (pop stk) (car (reverse stk)))
 (define (ret-pop stk) (reverse (cdr (reverse stk))))
+
+(define funs* (list (list "+" (list (arr 1 "Int")) (list (arr "a" "Int")))))
 
 (define (write-spec ls) 
   (if (list? ls) (begin (display "(") (map write-spec ls) (display ") "))
@@ -28,13 +31,22 @@
 
 (define (strcar s) (car (string->list s)))
 
+(define (call-fun f stk)
+  (if (not (member (v-val f) (map car funs*))) (printf "ERROR: function `~a' does not exist." (v-val f))
+      (let* ([fn (findf (λ (x) (equal? (car x) (v-val f))) funs*)] [sub (take (reverse stk) (length (third fn)))])
+        (displayln sub)
+        (if (member #f (map (λ (x y) (equal? (arr-type x) (v-type (car y)))) (third fn) sub)) (printf "ERROR: type mismatch.")
+            (append (take stk (- (length stk) (length sub))) (second fn))))))
+
 (define (lex l)
-  (cond [(or (char-numeric? (strcar l)) (char=? (strcar l) #\.)) (v l 'Int)]
-        [(char=? (strcar l) #\") (v l 'String)]
+  (cond [(or (char-numeric? (strcar l)) (char=? (strcar l) #\.)) (v l "Int")]
+        [(char=? (strcar l) #\") (v l "String")]
+        [(char=? (strcar l) #\#) (v l 'Type)]
         [else (v l 'Sym)]))
 
 (define (push~ stk s)
   (cond [(equal? (v-val s) "|") (push stk '())]
+        [(equal? (v-type s) 'Sym) (call-fun s stk)]
         [else (push (ret-pop stk) (push (pop stk) s))]))
 (define (process stk n)
   (if (empty? stk) n (process (cdr stk) (push~ n (car stk)))))
